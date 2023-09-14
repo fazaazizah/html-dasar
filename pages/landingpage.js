35e1d519992257507b545e24e84f8b8600201e21
Fazaazizah <fazashafa.ua@gmail.com>
2023-09-14 21:32:50 +0700

    Menambahkan landing dom

--- pages/landingpage.js
+++ pages/landingpage.js
@@ -26,3 +26,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function detail() {
+  confirm("Apakah anda ingin membeli?")
+}
